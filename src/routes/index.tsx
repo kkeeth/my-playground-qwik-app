07450ea7701476f5c2246be82771a07da1e8c38b
kkeeth <zensin0082@gmail.com>
2022-08-03 19:39:25 +0900

    feat: up to 'fetching resouce on state changes'

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { component$, Host, useStore } from "@builder.io/qwik";
+import { component$, Host, useStore, Resource, useResource$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
@@ -7,14 +7,26 @@ export default component$(() => {
     repos: ["qwik", "partytown"] as string[] | null,
   });
 
+  const reposResource = useResource$<string[]>(({ track, cleanup }) => {
+    track(github, 'org');
+
+    const controller = new AbortController();
+    cleanup(() => controller.abort());
+
+    return getRepositories(github.org, controller);
+  });
+
+  console.log('Render');
   return (
     <Host class="p-4">
       <h1 class="text-3xl mb-2" onClick$={() => console.warn("hola")}>
         Welcome to Qwik City
       </h1>
 
+      <Greet />
       <hr />
-
+      <Counter />
+      <hr />
       <div class="p-4">
         <span>
           GitHub organization:
@@ -28,40 +40,40 @@ export default component$(() => {
           />
         </span>
         <div>
-          {github.repos ? (
-            <ul class="list-square">
-              {github.repos.map((repo) => (
-                <li>
-                  <a
-                    class="underline text-blue-700"
-                    href={`https://github.com/${github.org}/${repo}`}
-                  >
-                    {github.org}/{repo}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            "loading..."
-          )}
+          <Resource
+            resource={reposResource}
+            onPending={() => <>Loading...</>}
+            onRejected={(error) => <>Error: {error.message}</>}
+            onResolved={(repos) => (
+              <ul class="list-square">
+                {repos.map((repo) => (
+                  <li>
+                    <a
+                      class="underline text-blue-700"
+                      href={`https://github.com/${github.org}/${repo}`}
+                    >{repo}</a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          />
         </div>
       </div>
-
-      <p>The meta-framework for Qwik.</p>
-      <button
-        class="bg-sky-500 text-white px-2 py-1 rounded-lg"
-        onClick$={() => alert("Hello World!!")}
-      >
-        Greet!
-      </button>
-
-      <hr />
-
-      <Counter />
     </Host>
   );
 });
 
+export const Greet = component$(() => (
+  <>
+    <p>The meta-framework for Qwik.</p>
+    <button
+      class="bg-sky-500 text-white px-2 py-1 rounded-lg"
+      onClick$={() => alert("Hello World!!")}
+    >
+      Greet!
+    </button>
+  </>
+))
 export const Counter = component$(() => {
   const store = useStore({ count: 0 });
 
@@ -80,4 +92,21 @@ export const Counter = component$(() => {
 
 export const head: DocumentHead = {
   title: "Welcome to Qwik City",
+};
+
+export const getRepositories = async (
+  username: string,
+  controller?: AbortController,
+): Promise<string[]> => {
+  console.log('FETCH', `https://api.github.com/users/${username}/repos`);
+
+  const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+    signal: controller?.signal,
+  });
+  console.log('FETCH resolved');
+  const json = await resp.json();
+
+  return Array.isArray(json)
+    ? json.map((repo: { name: string }) => repo.name)
+    : Promise.reject(json);
 };
