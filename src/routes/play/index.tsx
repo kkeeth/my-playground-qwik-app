537a2237325704833dcd87e365a12d8eb7f72423
kkeeth <zensin0082@gmail.com>
2022-08-11 16:08:39 +0900

    feat: listening global events with docment/window

--- src/routes/play/index.tsx
+++ src/routes/play/index.tsx
@@ -1,14 +1,18 @@
-import { component$, Host } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, Host, useStore } from "@builder.io/qwik";
 
 export default component$(() => {
+  const store = useStore({ x: 0, y: 0 });
+
   return (
     <Host>
       <h1>Welcome to the Docs!</h1>
+
+      <div document:onMouseMove$={(event: MouseEvent) => {
+        store.x = event.x;
+        store.y = event.y;
+      }}>
+        Your mouse location is ({store.x}, {store.y}).
+      </div>
     </Host>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome!",
-};
